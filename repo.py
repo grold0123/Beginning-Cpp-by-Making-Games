9dd9e485b9c684508a55103431f18e83ebef68de
grold0123 <172453552+grold0123@users.noreply.github.com>
2025-10-24 11:21:43 +0800

    update

--- repo.py
+++ repo.py
@@ -4,15 +4,15 @@ import subprocess,pathlib,os
 base_path = pathlib.Path()
 
 args = [
-    ['git','add','C:/projects/Beginning-Cpp-by-Making-Games'],#add
-    ['git','commit','-m','update'],#commit
-    ['git','push'],#push
+    ('adding',['git','add','C:/projects/Beginning-Cpp-by-Making-Games']),#add
+    ('committing',['git','commit','-m','update']),#commit
+    ('pushing',['git','push']),#push
 ]
 
 os.system('cls')
 print('\n\nUpdating repository\n\n')
-for arg in args:
+for msg,arg in args:
     print('\n\n')
     subprocess.run(arg)
-    print('\n\nsuccesful\n\n')
+    print(f'\n\n{msg} succesful\n\n')
 print('\n\nRepository successfully updated\n\n')
