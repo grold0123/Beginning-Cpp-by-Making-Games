404fd4cfc044167bc5266013709fb2bfa26a1858
grold0123 <172453552+grold0123@users.noreply.github.com>
2025-10-26 12:18:18 +0800

    update

--- src/instructions/main.cpp
+++ src/instructions/main.cpp
@@ -3,6 +3,13 @@ instructions
 
 **************************************************/
 #include<slow_print.h>
+
 int main () {
-slow_print("Hello world");
+instructions();
+}
+
+
+void instructions(){
+    slow_print("\nWelcome to the most fun you've ever had with text!\n\n");
+    slow_print("Here's how to play the game...\n");
 }
